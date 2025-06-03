186ac9988716e190b0f7ecc991f5f862e61c2f88
Tim Voelker <voelker.timothyj@gmail.com>
2025-06-03 00:16:48 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ df = pd.read_csv('vehicle_service_data.csv')  # Example file name
 
 # Sidebar filters
 
-df['Date'] = pd.to_datetime(df['Date'])
-df['Month'] = df['SERIVCE_DATE'].dt.month_name()
+df['SERIVCE_DATE'] = pd.to_datetime(df['SERIVCE_DATE'])
+df['Month'] = df['SERVICE_DATE'].dt.month_name()
 
 st.sidebar.header("Filter Options")
 month = st.sidebar.selectbox("Select Month", df['Month'].unique())
