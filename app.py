f994ad497766c33ffaec74993a6e2e7fd4503774
Tim Voelker <voelker.timothyj@gmail.com>
2025-06-04 01:06:22 -0400

    Update app.py

--- app.py
+++ app.py
@@ -126,6 +126,7 @@ if st.button("Generate PDF Summary"):
 
     pdf_bytes = pdf.output(dest='S').encode('latin1')
     b64 = base64.b64encode(pdf_bytes).decode()
+    
     href = f'<a href="data:application/octet-stream;base64,{b64}" download="auto_shop_summary.pdf">Download Report PDF</a>'
     st.markdown(href, unsafe_allow_html=True)
     
